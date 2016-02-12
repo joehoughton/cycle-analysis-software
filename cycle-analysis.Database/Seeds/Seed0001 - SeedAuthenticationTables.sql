2701eb3f30c917e97d863f61af8876e0ef43cc80
Joe Houghton <joe.houghton@houghtonjoe.com>
2016-02-12 23:23:45 +0000

    Added DbUp to project to deploy database changes.
    Created and seeded user tables with DbUp.

---
+++ cycle-analysis.Database/Seeds/Seed0001 - SeedAuthenticationTables.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT [dbo].[Role] ON 
+
+INSERT [dbo].[Role] ([Id], [Name]) VALUES (1, N'Admin')
+SET IDENTITY_INSERT [dbo].[Role] OFF
+SET IDENTITY_INSERT [dbo].[User] ON 
+
+INSERT [dbo].[User] ([Id], [Username], [Email], [HashedPassword], [Salt], [IsLocked], [DateCreated]) VALUES (1019, N'joehoughton', N'joehoughton@email.com', N'C/QQLW7Awf7sJGTr36rxkxq62SbSoIpzm8jaVpfIgu8=', N'fs4OFBGmFF9y2Urq2iUk0Q==', 0, CAST(N'2015-12-28 17:22:02.217' AS DateTime))
+SET IDENTITY_INSERT [dbo].[User] OFF
+SET IDENTITY_INSERT [dbo].[UserRole] ON 
+
+INSERT [dbo].[UserRole] ([Id], [UserId], [RoleId]) VALUES (1008, 1019, 1)
+SET IDENTITY_INSERT [dbo].[UserRole] OFF
